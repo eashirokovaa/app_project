ce47f577b1b813422e8ffe8593161bbcb05d62cc
eashirokovaa <eashirokovaa@gmail.com>
2023-02-19 19:36:57 +0300

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import data from "./data.json";
 import WordRow from "./components/WordsList";
-import EditableRow from "./components/EditableRow";
+import Card from "./components/WordCard";
 
 function App() {
   return (
     <div className="app-container">
+      <div className="cardRow">
+      {data.map((word, index) => (
+            <>
+              <Card
+                key={index}
+                word = {word}
+              />
+            </>
+          ))}
+      </div>
       <table>
         <thead>
           <th>English word</th>
---
+++ my-app/src/components/WordCard.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import style from "/Users/katasirokova/app_project/my-app/src/components/styles/card.css";
+
+function Card(props) {
+  const { english, transcription, russian } = props.word;
+  const [pressed, setPressed] = useState(true);
+  const handlePressed = () => {
+      setPressed(!pressed);
+  };
+  return (
+    <div className="card">
+      <h2 className="hedding">{english}</h2>
+      <p className="transcription">{transcription}</p>
+      {pressed ? (
+        <Button className="translate" variant="success" onClick={handlePressed}>
+          {" "}
+          Translate
+        </Button>
+      ) : (
+        <p>{russian}</p>
+      )}
+    </div>
+  );
+}
+export default Card;
